a18d497e734815a8b4f68ad55cd9c4cb1c6a22c7
JackOXI <53652452+JackW6809@users.noreply.github.com>
2024-03-04 21:47:49 +0000

    Update and rename upgrade-rocky-8.sh to upgrade-centos-7.sh

--- upgrade-centos-7.sh
+++ upgrade-centos-7.sh
@@ -1,5 +1,5 @@
 yum install -y http://repo.almalinux.org/elevate/elevate-release-latest-el$(rpm --eval %rhel).noarch.rpm
-yum install -y leapp-upgrade leapp-data-rockylinux
+yum install -y leapp-upgrade leapp-data-rocky
 leapp preupgrade
 rmmod pata_acpi && echo PermitRootLogin yes | tee -a /etc/ssh/sshd_config && leapp answer --section remove_pam_pkcs11_module_check.confirm=True
 leapp upgrade 
